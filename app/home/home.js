78b0fc47a738e3d2e58698aa34e409cb7850a007
suneetsasidharan <suneet.sasidharan@gmail.com>
2016-02-22 02:07:46 -0800

    adding dynamic fields

--- app/home/home.js
+++ app/home/home.js
@@ -5,10 +5,22 @@ angular.module('myApp.home', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {
     templateUrl: 'home/home.html',
-    controller: 'View1Ctrl'
+    controller: 'HomeCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('HomeCtrl', ['$scope', function($scope) {
+        $scope.triggers = [{id:'trigger1'}];
+
+
+        $scope.addTrigger =  function(){
+            var newItemNo = $scope.triggers.length+1;
+            $scope.triggers.push({'id':'trigger'+newItemNo});
+    };
+
+        $scope.removeTrigger = function() {
+            var lastItem = $scope.triggers.length-1;
+            $scope.triggers.splice(lastItem);
+        };
 
 }]);
